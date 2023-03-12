cf645d6dbd78a7734a08098109d158ac35bcafc3
smile1130 <superskiller1130@gmail.com>
2023-03-13 03:10:27 +0900

    fix title error231 and footer errror22

---
+++ programs/Shred_Staking/src/constants.rs
@@ -0,0 +1,10 @@
+pub const STAKE_MAX_COUNT: usize = 1000;
+
+pub const GLOBAL_AUTHORITY_SEED: &str = "global-authority";
+pub const EPOCH: i64 = 60;                          // 86400 - 1 day
+pub const FACTOR: u64 = 125;                            // X 1.25 Reward
+pub const NORMAL_REWARD_AMOUNT: u64 = 100_000 * 10;     // 10 $WHEY
+pub const LEGENDARY_REWARD_AMOUNT: u64 = 100_000 * 25;  // 25 $WHEY
+
+pub const REWARD_TOKEN_MINT_PUBKEY: &str = "8EoML7gaBJsgJtepm25wq3GuUCqLYHBoqd3HP1JxtyBx";
+pub const MIN_REWARD_DEPOSIT_AMOUNT: u64 = 10_000_000_000;
